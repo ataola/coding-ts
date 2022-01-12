9cc781d3f0ada18d949a2d6961daec993b46146c
ataola <zjt613@gmail.com>
2022-01-12 20:11:23 +0800

    ✨ feat: leetcode two-sum

---
+++ code/base/interface.d.ts
@@ -0,0 +1,15 @@
+export interface objectStringType {
+  [key: string]: string;
+}
+
+export interface objectNumberType {
+  [key: string]: number;
+}
+
+export interface objectBooleanType {
+  [key: string]: boolean;
+}
+
+export interface objectAnyType {
+  [key: string]: any;
+}
---
+++ code/base/type.d.ts
@@ -0,0 +1,15 @@
+export type objectStringType = {
+  [key: string]: string;
+};
+
+export type objectNumberType = {
+  [key: string]: number;
+};
+
+export type objectBooleanType = {
+  [key: string]: boolean;
+};
+
+export type objectAnyType = {
+  [key: string]: any;
+};
