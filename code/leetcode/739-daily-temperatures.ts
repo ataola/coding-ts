457519b251e4826d9d9e7a150069f9b828c8a25a
ataola <zjt613@gmail.com>
2022-01-17 18:43:27 +0800

    ✨ feat: daily-temperatures

---
+++ code/leetcode/739-daily-temperatures.ts
@@ -0,0 +1,20 @@
+// code/leetcode/739-daily-temperatures.ts
+
+/**
+ * 
+ * @param {number[]} temperatures
+ * @return {number[]} 
+ */
+export function dailyTemperatures(temperatures: number[]): number[] {
+  const len = temperatures.length;
+  const res: number[] = new Array(len).fill(0);
+  const stack: number[] = [];
+  for (let i = 0; i < len; i++) {
+    while (stack.length && temperatures[i] > temperatures[stack[stack.length - 1]]) {
+      const top = stack.pop();
+      res[top as number] = i - (top as number);
+    }
+    stack.push(i);
+  }
+  return res;
+};
