a30678489c53051368e81f88d64168e394c40bbe
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2022-02-11 08:40:57 +0000

    Deploy ataola/coding-ts to github.com/ataola/coding-ts.git:gh-pages

--- code/sum.ts
+++
@@ -1,4 +0,0 @@
-// code/sum.ts 两数之和测试案例
-type sumType = (a: number, b: number) => number;
-const sum: sumType = (a, b) => a + b;
-export default sum;
--- test/leetcode/322-coin-change.test.ts
+++
@@ -1,8 +0,0 @@
-// test/leetcode/322-coin-change.test.ts
-import { coinChange } from '../../code/leetcode/322-coin-change';
-
-describe('test function coinChange:', () => {
-  test('test case coins = [1, 2, 5], amount = 11', () => {
-    expect(coinChange([1, 2, 5], 11)).toBe(3);
-  });
-});
